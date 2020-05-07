ec8001690bfa01139546a15699037531f3d1c528
qburnsFB <quinton@giddy.io>
2020-05-07 13:21:36 -0400

    Include usePalette and useColor in exports to use like documentation suggests.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import Color from './Color';
 import Palette from './Palette';
 import getColor from './Color/getColor';
 import getPalette from './Palette/getPalette';
-
+import usePalette from "./Palette/usePalette";
+import useColor from "./Color/useColor";
 export {
-  getColor, Color, getPalette, Palette,
+  getColor, Color, getPalette, Palette, usePalette, useColor
 };
 
 export default Color;
